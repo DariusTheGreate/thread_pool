714ede70e5a9652ec3778705cedd4a894c89939c
SatosiNakamoto <kotovsshikov31415926@gmail.com>
2020-10-09 22:15:42 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "thread_queue.h"
+
+void main()
+{
+	
+}
---
+++ thread_queue.h
@@ -0,0 +1,12 @@
+#ifndef thread_queue
+#define thread_queue
+
+#include <mutex>
+#include <queue>
+
+class thread_queue
+{
+
+};
+
+#endif
