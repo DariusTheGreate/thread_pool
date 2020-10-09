cf3f42b9fa7cd56dcbe0388acbcb600f0e9a6ebf
SatosiNakamoto <kotovsshikov31415926@gmail.com>
2020-10-09 22:45:59 +0300

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include "thread_queue.h"
+#include <iostream>
 
 void main()
 {
-	
+	thread_queue<int> q;
+	int item = 5;
+	q.enqueue(item);
+	q.enqueue(item);
+	q.enqueue(item);
+	q.enqueue(item);
+	std:: cout << q.dequeue(); 
 }
--- thread_queue.h
+++ thread_queue.h
@@ -1,12 +1,36 @@
-#ifndef thread_queue
-#define thread_queue
+#ifndef thread_queue_
+#define thread_queue_
 
 #include <mutex>
 #include <queue>
 
+template <typename T> 
 class thread_queue
 {
+private:
+  std::queue<T> t_queue;
+  std::mutex t_mutex;
+public:
+  bool empty() {
+    std::unique_lock<std::mutex> lock(t_mutex);
+    return t_queue.empty();
+  }
+  
+  int size() {
+    std::unique_lock<std::mutex> lock(t_mutex);
+    return t_queue.size();
+  }
 
+  void enqueue(T& t) {
+    std::unique_lock<std::mutex> lock(t_mutex);
+    t_queue.push(t);
+  }
+  
+  T& dequeue() {
+    std::unique_lock<std::mutex> lock(t_mutex);
+    return t_queue.front();
+    
+  }
 };
 
 #endif
